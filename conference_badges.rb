daa1d9159ae3c159dbe0dde08ecf4adb8ef0a044
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-01 01:40:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ end
 def assign_rooms(names)
   my_arr = []
   (0..6).each do |room|
-    push.my_arr("Hello, #{names[room}! You'll be assigned to room #{room}!")
+    my_arr.push("Hello, #{names[room}! You'll be assigned to room #{room}!")
   end
   my_arr
 end
