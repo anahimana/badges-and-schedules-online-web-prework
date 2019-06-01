c723876f01d92f543f832147879aec3f6f902e8e
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-01 01:25:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  my_arr = []
+  names.each do |name|
+    my_arr.push(badge_maker(name))
+  end
+  my_arr
+end
+
+def assign_rooms(names)
+  names
+end
